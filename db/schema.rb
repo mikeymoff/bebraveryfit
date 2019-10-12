c2f2091d2b691061cad2364b9b4ea89c019c80d7
Mike Moffitt <mike@mikes-mbp.lan>
2019-10-12 10:09:19 -0600

    added routes and Controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_153054) do
+ActiveRecord::Schema.define(version: 2019_10_12_152053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
